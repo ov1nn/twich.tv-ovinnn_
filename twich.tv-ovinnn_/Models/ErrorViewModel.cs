2af85d502fb3f99a4370e6c796fe365a4dd69df5
ovin <ovin@DESKTOP-PQRKILF>
2025-05-17 01:32:01 +0300

    Добавьте файлы проекта.

---
+++ twich.tv-ovinnn_/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace twich.tv_ovinnn_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
